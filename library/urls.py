63ce9c5edaa0d8bd9e209226db442d2932b09208
gabriel santos <gabrielsantos98898@gmail.com>
2025-07-15 13:39:09 -0300

    :sparkles: Criado a visualização de livros e os detalhes de um livro

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'library'
+
 urlpatterns = [
-    
+    path('books/', views.view_books, name='view_books'),
+    path('books/<int:book_id>/', views.details_book, name='details_book'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'library/Book/view_books.html', context={'books': books})
+
+def details_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'library/Book/details_book.html', context={'book': book})

1efc5094b090d35667e4f07e52a98792bbc55254
gabriel santos <gabrielsantos98898@gmail.com>
2025-07-17 15:31:45 -0300

    :sparkles: Criado tela sobre, com novo visual

--- Home/views.py
+++ Home/views.py
@@ -5,3 +5,9 @@ def home(request):
     Render the home page.
     """
     return render(request, 'Home/home.html')
+
+def about(request):
+    """
+    Render the about page.
+    """
+    return render(request, 'Home/about.html')

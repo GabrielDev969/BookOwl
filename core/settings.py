ebe6ff658edc0059733429349da3bc339863817c
gabriel santos <gabrielsantos98898@gmail.com>
2025-07-18 14:09:27 -0300

    :wrench: Ajustes

--- core/settings.py
+++ core/settings.py
@@ -42,8 +42,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -137,7 +136,6 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATICFILES_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

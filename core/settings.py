8f00fa198f93a9af7ac93d7215fe12a8e4201a4b
gabriel santos <gabrielsantos98898@gmail.com>
2025-07-18 11:59:40 -0300

    :sparkles: Adicionado configurações paara produção

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -133,6 +134,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATICFILES_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

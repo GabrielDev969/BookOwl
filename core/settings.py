68e5df9f86d0a2c3d1124673928ca8958da1fce4
gabriel santos <gabrielsantos98898@gmail.com>
2025-07-18 14:49:12 -0300

    :wrench: Ajustado DEBUG produciton

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 # --- CONFIGURAÇÕES DE REDE (HOSTS) ---
 # Configuração dinâmica de ALLOWED_HOSTS para Railway.
